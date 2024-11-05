d2a963a55f1d6f94defc91c51198812733c91b55
Ankit Kumar <ankitkumar16729@gmail.com>
2024-11-05 19:30:03 +0530

    Create Majority Element.cpp

---
+++ Majority Element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // moores voting algorithm
+        int n=nums.size();
+        int freq=0;
+        int ans=0;
+        for(int i=0; i<n; i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(nums[i]==ans){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
