a58af845abffa5f14ba92167cac1e5fc688c173e
Ankit Kumar <ankitkumar16729@gmail.com>
2025-02-18 19:00:21 +0530

    Update move zeroes.cpp

--- move zeroes.cpp
+++ move zeroes.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int nonzero=0;
-        for(int j=0; j<nums.size(); j++){
-            if(nums[j]!=0){
-                swap(nums[j],nums[nonzero]);
-                nonzero++;
-            }
+    int j = -1;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i] == 0){
+            j = i;
+            break;
         }
+    }
+    if(j == -1) return;
+    for(int i= j+1; i<nums.size(); i++){
+        if(nums[i]!=0){
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
     }
 };
