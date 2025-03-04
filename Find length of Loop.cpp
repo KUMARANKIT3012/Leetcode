027adb059b942cff3e30d78e45a931a2869e6deb
Ankit Kumar <ankitkumar16729@gmail.com>
2025-03-04 18:43:17 +0530

    Create Find length of Loop.cpp

---
+++ Find length of Loop.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    // Function to find the length of a loop in the linked list.
+    int findlength(Node * slow, Node * fast){
+        int count = 1;
+        fast = fast->next;
+        while(slow != fast){
+            count++;
+            fast= fast->next;
+        }
+        return count;
+    }
+    int countNodesinLoop(Node *head) {
+        // Code here
+        Node * slow = head;
+        Node * fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow= slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                return findlength(slow, fast);
+            }
+        }
+        return 0;
+    }
+};
