9661902ac4e78652e0728f44be9708f3723d4e63
Ankit Kumar <ankitkumar16729@gmail.com>
2025-03-07 16:31:27 +0530

    Create Delete all occurrences of a given key in a doubly linked list.cpp

---
+++ Delete all occurrences of a given key in a doubly linked list.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    void deleteAllOccurOfX(struct Node** head_ref, int x) {
+        if (*head_ref == NULL) return;  // Edge case: Empty list
+
+        Node* temp = *head_ref;
+
+        while (temp != NULL) {
+            if (temp->data == x) {
+                Node* nextnode = temp->next;
+                Node* prevnode = temp->prev;
+
+                // If the node to delete is the head node
+                if (temp == *head_ref) {
+                    *head_ref = nextnode;  // Update head reference
+                }
+
+                // Adjust the previous and next pointers
+                if (nextnode) nextnode->prev = prevnode;
+                if (prevnode) prevnode->next = nextnode;
+
+                delete temp;  // Free memory
+                temp = nextnode;  // Move to the next node
+            } 
+            else {
+                temp = temp->next;
+            }
+        }
+    }
+};
