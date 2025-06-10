694c52c5138f11e4b18900163f4ee3fc6da2040b
Ankit Kumar <ankitkumar16729@gmail.com>
2025-06-10 20:59:21 +0530

    Update Single Element in a Sorted Array.cpp

--- Single Element in a Sorted Array.cpp
+++ Single Element in a Sorted Array.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& A) {
-        int n=A.size();
-        if(n==1){return A[0];}
-        int st=0;
-        int end=n-1;
-        while(st<=end){
-            int mid=st+(end-st)/2;
-            if(mid==0 && A[0]!=A[1]){
-                return A[mid];
-                }
-            if(mid==n-1 && A[n-1]!=A[n-2]){
-                return A[mid];
-                }
-            if(A[mid-1]!=A[mid] && A[mid]!=A[mid+1]){
-                return A[mid];
+        int st = 0;
+        int end = A.size()-1;
+        while(st<end){
+            int mid = st + (end - st)/2;
+            if(mid % 2 == 1) mid--;
+            if(A[mid] == A[mid+1]){
+                st = mid+2;
             }
-            else if(mid%2==0){ // even (remember this case as well as......
-                if(A[mid-1]==A[mid]){ // left
-                    end=mid-1;
-                }
-                else{ // right
-                    st=mid+1;
-                }
-            }
-            else{ // odd ....... this case)
-                if(A[mid-1]==A[mid]){ // left
-                    st=mid+1;
-                }
-                else{ // right
-                    end=mid-1;
-                }
+            else{
+                end = mid;
             }
         }
-        return -1;
+        return A[st];
     }      
 };
