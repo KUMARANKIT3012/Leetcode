044663a2a9408a55fa4fdaf698878321e4cb3310
Ankit Kumar <ankitkumar16729@gmail.com>
2025-07-23 20:43:41 +0530

    Update Binary Tree Inorder Traversal.cpp

--- Binary Tree Inorder Traversal.cpp
+++ Binary Tree Inorder Traversal.cpp
@@ -43,3 +43,37 @@ public:
         return inorder;
     }
 };
+
+// Morris Traversal
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode* cur = root;
+
+        while (cur != NULL) {
+            if (cur->left == NULL) {
+                inorder.push_back(cur->val);
+                cur = cur->right;
+            } else {
+                TreeNode* prev = cur->left;
+                while (prev->right != NULL && prev->right != cur) {
+                    prev = prev->right;
+                }
+
+                if (prev->right == NULL) {
+                    prev->right = cur;  // Threading
+                    cur = cur->left;
+                } else {
+                    prev->right = NULL;  // Remove thread
+                    inorder.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+
+        return inorder;
+    }
+};
+
