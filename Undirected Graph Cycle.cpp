84cedd1d7a7503fbf35b2f987f0d7ed468ecc1f5
Ankit Kumar <ankitkumar16729@gmail.com>
2025-08-02 13:20:51 +0530

    Update Undirected Graph Cycle.cpp

--- Undirected Graph Cycle.cpp
+++ Undirected Graph Cycle.cpp
@@ -1,3 +1,4 @@
+// using BFS
 class Solution {
   public:
     bool detect(int src, vector<int> adj[], int vis[]) {
@@ -45,5 +46,42 @@ class Solution {
         }
 
         return false; // No cycle found in any component
+    }
+};
+
+
+
+
+// using DFS: 
+class Solution {
+private:
+    bool dfs(int node, int parent, vector<int>& vis, vector<int> adj[]) {
+        vis[node] = 1;
+
+        for (auto adjacentNode : adj[node]) {
+            if (!vis[adjacentNode]) {
+                if (dfs(adjacentNode, node, vis, adj) == true)
+                    return true;
+            } else if (adjacentNode != parent) {
+                return true; // Cycle detected
+            }
+        }
+
+        return false; // No cycle found
+    }
+
+public:
+    // Function to detect cycle in an undirected graph
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<int> vis(V, 0); // visited array
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                if (dfs(i, -1, vis, adj) == true)
+                    return true;
+            }
+        }
+
+        return false; // No cycle in any component
     }
 };
