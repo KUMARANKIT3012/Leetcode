fdeb8f5071681c8495a2bc1cc61c1c112930ffbb
Ankit Kumar <ankitkumar16729@gmail.com>
2025-08-15 13:13:05 +0530

    Create Number of Operations to Make Network Connected.cpp

---
+++ Number of Operations to Make Network Connected.cpp
@@ -0,0 +1,74 @@
+class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        if (pu == pv) return;
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        } else if (rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        } else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+        if (pu == pv) return;
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n - 1) return -1; // Not enough edges to connect all nodes
+        
+        DisjointSet ds(n);
+        int cntExtras = 0;
+
+        for (auto it : connections) {
+            int u = it[0];
+            int v = it[1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                cntExtras++;
+            } else {
+                ds.unionBySize(u, v);
+            }
+        }
+
+        int cntC = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent[i] == i) cntC++;
+        }
+
+        int ans = cntC - 1;
+        if (cntExtras >= ans) return ans;
+        return -1;
+    }
+};
