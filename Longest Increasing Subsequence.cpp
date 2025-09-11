55691bfd0082e2ae5005aa2d0a8c7cb82b127b48
Ankit Kumar <ankitkumar16729@gmail.com>
2025-09-11 20:58:42 +0530

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -20,3 +20,26 @@ public:
         return f(0, -1, nums, n, dp);
     }
 };
+
+
+
+// TABULATION - 
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1); // every element is at least length 1
+        int maxi = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int prev = 0; prev < i; prev++) {
+                if (nums[prev] < nums[i]) {
+                    dp[i] = max(dp[i], 1 + dp[prev]);
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+
+        return maxi;
+    }
+};
