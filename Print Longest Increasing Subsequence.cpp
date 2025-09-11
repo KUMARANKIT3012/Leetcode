d63d6aa4ca1703f5cf1c70f915e18545be04fd15
Ankit Kumar <ankitkumar16729@gmail.com>
2025-09-11 21:00:51 +0530

    Create Print Longest Increasing Subsequence.cpp

---
+++ Print Longest Increasing Subsequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> getLIS(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> dp(n, 1), hash(n);
+        int maxi = 1;
+        int lastIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            hash[i] = i;
+            for (int prev = 0; prev < i; prev++) {
+                if (arr[prev] < arr[i] && 1 + dp[prev] > dp[i]) {
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
+                }
+            }
+            if (dp[i] > maxi) {
+                maxi = dp[i];
+                lastIndex = i;
+            }
+        }
+
+        // reconstruct LIS
+        vector<int> lis;
+        lis.push_back(arr[lastIndex]);
+        while (hash[lastIndex] != lastIndex) {
+            lastIndex = hash[lastIndex];
+            lis.push_back(arr[lastIndex]);
+        }
+
+        reverse(lis.begin(), lis.end());
+        return lis;
+    }
+};
