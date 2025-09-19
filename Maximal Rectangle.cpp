9f4fb4d9a248463c3e48b0b1d8087f4a16d33db1
Ankit Kumar <ankitkumar16729@gmail.com>
2025-09-19 18:48:24 +0530

    Update Maximal Rectangle.cpp

--- Maximal Rectangle.cpp
+++ Maximal Rectangle.cpp
@@ -43,3 +43,61 @@ private:
         return maxArea;
     }
 };
+
+
+
+
+
+// DP - 
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+
+        int rows = matrix.size(), cols = matrix[0].size();
+        vector<int> height(cols, 0);
+        vector<int> left(cols, 0);
+        vector<int> right(cols, cols);
+        int maxArea = 0;
+
+        for (int i = 0; i < rows; i++) {
+            int curLeft = 0, curRight = cols;
+
+            // 1. Update heights
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
+            }
+
+            // 2. Update left boundaries
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1')
+                    left[j] = max(left[j], curLeft);
+                else {
+                    left[j] = 0;
+                    curLeft = j + 1;
+                }
+            }
+
+            // 3. Update right boundaries
+            for (int j = cols - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1')
+                    right[j] = min(right[j], curRight);
+                else {
+                    right[j] = cols;
+                    curRight = j;
+                }
+            }
+
+            // 4. Compute area
+            for (int j = 0; j < cols; j++) {
+                maxArea = max(maxArea, (right[j] - left[j]) * height[j]);
+            }
+        }
+
+        return maxArea;
+    }
+};
+
