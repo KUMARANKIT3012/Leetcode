f78c1672e6692d6b963f0f6b96db84dbd86bdc0d
Ankit Kumar <ankitkumar16729@gmail.com>
2025-10-18 10:33:29 +0530

    Refactor majorityElement function for clarity

--- Majority Element II.cpp
+++ Majority Element II.cpp
@@ -2,33 +2,34 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int count1 = 0, count2 = 0;
-        int element1 = INT_MIN, element2 = INT_MIN;
-        for(int i = 0; i<nums.size(); i++){
-            if(count1 == 0 && nums[i] != element2){
-                count1 = 1;
-                element1 = nums[i];
+        int freq1 = 0;
+        int freq2 = 0;
+        int ans1 = INT_MIN, ans2 = INT_MIN;
+        for(int i = 0; i<n; i++){
+            if(freq1 == 0 && nums[i] != ans2){
+                freq1 = 1;
+                ans1 = nums[i];
             }
-            else if(count2 == 0 && nums[i] != element1){
-                count2 = 1;
-                element2 = nums[i];
+            else if(freq2 == 0 && nums[i] != ans1){
+                freq2 = 1;
+                ans2 = nums[i];
             }
-            else if(element1 == nums[i]) count1++;
-            else if(element2 == nums[i]) count2++;
+            else if(ans1 == nums[i]) freq1++;
+            else if(ans2 == nums[i]) freq2++;
             else{
-                count1--;
-                count2--;
+                freq1--;
+                freq2--;
             }
         }
         vector<int> ls;
-        count1 = 0, count2 = 0;
-        for(int i = 0; i<nums.size(); i++){
-            if(element1 == nums[i]) count1++;
-            if(element2 == nums[i]) count2++;
+        freq1 = 0 , freq2 = 0;
+        for(int i = 0; i<n; i++){
+            if(ans1 == nums[i]) freq1++;
+            if(ans2 == nums[i]) freq2++;
         }
-        int mini = (int)(n/3) + 1;
-        if(count1 >= mini) ls.push_back(element1);
-        if(count2 >= mini) ls.push_back(element2);
+        int mini = (int)(n/3)+1;
+        if(freq1 >= mini) ls.push_back(ans1);
+        if(freq2 >= mini) ls.push_back(ans2);
         sort(ls.begin(), ls.end());
         return ls;
     }
