9d302ce89aa247cd3956b7482c64f6979ee580b3
Ankit Kumar <ankitkumar16729@gmail.com>
2025-10-26 15:17:12 +0530

    Replace minRow function with rowWithMax1s

--- Row with Minimum 1s.cpp
+++ Row with Minimum 1s.cpp
@@ -1,26 +1,24 @@
+// User function template for C++
 class Solution {
   public:
-    int minRow(vector<vector<int>> &mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-
-        int minCount = m + 1;
-        int minRowIndex = -1;
-
-        for (int i = 0; i < n; i++) {
-            int onesCount = 0;
-
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 1)
-                    onesCount++;
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        // code here
+        int n = arr.size();
+        int m = arr[0].size();
+        
+        int maxcount = -1;
+        int index = -1;
+        for(int i = 0; i<n; i++){
+            int countrow = 0;
+            for(int j = 0 ;j<m; j++){
+                countrow+=arr[i][j];
             }
-
-            if (onesCount < minCount) {
-                minCount = onesCount;
-                minRowIndex = i;
+            if(countrow > maxcount){
+                maxcount = countrow;
+                index = i;
             }
+            
         }
-
-        return minRowIndex + 1; // rows are 1-indexed
+        return index;
     }
 };
