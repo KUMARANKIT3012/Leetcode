5dbdee5153d71a3fa6822ce2322a2796bcabb242
Ankit Kumar <ankitkumar16729@gmail.com>
2025-10-27 11:02:40 +0530

    Optimize isIsomorphic with vector mappings
    Refactor isIsomorphic function to use vectors for mapping characters instead of unordered_map.

--- Isomorphic Strings.cpp
+++ Isomorphic Strings.cpp
@@ -3,16 +3,21 @@ public:
     bool isIsomorphic(string s, string t) {
         if (s.size() != t.size()) return false;
 
-        unordered_map<char, char> mapST, mapTS;
+        vector<int> mapS(256, -1); // maps s -> t
+        vector<int> mapT(256, -1); // maps t -> s
 
         for (int i = 0; i < s.size(); i++) {
-            char c1 = s[i], c2 = t[i];
+            char c1 = s[i];
+            char c2 = t[i];
 
-            if (mapST.count(c1) && mapST[c1] != c2) return false;
-            if (mapTS.count(c2) && mapTS[c2] != c1) return false;
-
-            mapST[c1] = c2;
-            mapTS[c2] = c1;
+            // if already mapped, check if consistent
+            if (mapS[c1] == -1 && mapT[c2] == -1) {
+                mapS[c1] = c2;
+                mapT[c2] = c1;
+            }
+            else if (mapS[c1] != c2 || mapT[c2] != c1) {
+                return false;
+            }
         }
         return true;
     }
