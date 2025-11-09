4d5abb849a45780909bc278bebddfea4f158eb6e
Ankit Kumar <ankitkumar16729@gmail.com>
2025-11-09 15:10:34 +0530

    Add comments to deleteNode function
    Updated the deleteNode function to include comments explaining the logic behind the node deletion process.

--- Delete Node in a Linked List.cpp
+++ Delete Node in a Linked List.cpp
@@ -3,7 +3,9 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
 
+        // node's value will directly point to it's next value 
         node->val = node->next->val; 
+        // and node's next value will point to it's next to next value 
         node->next = node->next->next;
         
         
