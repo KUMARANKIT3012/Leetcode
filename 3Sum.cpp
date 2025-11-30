6d7934ed672315b670da7197b9ec0bf9569589e7
Ankit Kumar <ankitkumar16729@gmail.com>
2025-11-30 13:33:41 +0530

    Refactor 3Sum algorithm for duplicates and optimization
    Fixed logic for handling duplicates and improved optimization in the 3Sum algorithm.

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -4,32 +4,28 @@ public:
         int n = nums.size();
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
+        for(int i = 0; i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
 
-        for (int i = 0; i < n; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            // Optimization: stop if nums[i] > 0
-            if (nums[i] > 0) break;
-
-            int j = i + 1;
-            int k = n - 1;
-
-            while (j < k) {
+            if(nums[i] > 0) break; // forgot this 
+            int j = i+1;
+            int k = n-1;
+            while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum > 0) k--;
-                else if (sum < 0) j++;
-                else {
-                    ans.push_back({nums[i], nums[j], nums[k]});
+                if(sum < 0){
                     j++;
-                    k--;
-
-                    // Skip duplicates for j
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    // Skip duplicates for k
-                    while (j < k && nums[k] == nums[k + 1]) k--;
                 }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++; // here j and k will move or else woh infinite loop mein chala jaega 
+                    k--; 
+                    while(j < k && nums[j] == nums[j-1]) j++; // continue will not come here 
+                    while(j < k && nums[k] == nums[k + 1]) k--; // this while loop will be inside the sum == k wala condition 
+                    // ans it will be k+1 not k-1 in the above equation 
+                }   
             }
         }
         return ans;
