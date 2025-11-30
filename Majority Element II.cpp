a3124ea8e825a630951ef1b8f0ff6bc1a65ea37f
Ankit Kumar <ankitkumar16729@gmail.com>
2025-11-30 12:15:18 +0530

    Refactor majorityElement logic and fix counting errors
    Refactor majorityElement function to use counters for elements and correct logic for counting occurrences.

--- Majority Element II.cpp
+++ Majority Element II.cpp
@@ -2,35 +2,46 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int freq1 = 0;
-        int freq2 = 0;
-        int ans1 = INT_MIN, ans2 = INT_MIN;
-        for(int i = 0; i<n; i++){
-            if(freq1 == 0 && nums[i] != ans2){
-                freq1 = 1;
-                ans1 = nums[i];
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
+        for(int i = 0 ; i<n; i++){
+            if(cnt1 == 0 && nums[i] != el2){
+                cnt1 = 1;
+                el1 = nums[i];
             }
-            else if(freq2 == 0 && nums[i] != ans1){
-                freq2 = 1;
-                ans2 = nums[i];
+            else if(cnt2 == 0 && nums[i] != el1){
+                cnt2 = 1;
+                el2 = nums[i];
             }
-            else if(ans1 == nums[i]) freq1++;
-            else if(ans2 == nums[i]) freq2++;
+
+            else if(el1 == nums[i]) cnt1++;
+            else if(el2 == nums[i]) cnt2++;
             else{
-                freq1--;
-                freq2--;
+                cnt1--; // here i have done the mistake 
+                cnt2--; // here also 
             }
         }
+
+
         vector<int> ls;
-        freq1 = 0 , freq2 = 0;
-        for(int i = 0; i<n; i++){
-            if(ans1 == nums[i]) freq1++;
-            if(ans2 == nums[i]) freq2++;
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i =0 ; i<n ;i++){
+            if(nums[i] == el1) cnt1++; // here 
+            else if(nums[i] == el2){ // here also put the condition 
+                cnt2++;
+            }
         }
+
         int mini = (int)(n/3)+1;
-        if(freq1 >= mini) ls.push_back(ans1);
-        if(freq2 >= mini) ls.push_back(ans2);
-        sort(ls.begin(), ls.end());
+        if(cnt1 >= mini){
+            ls.push_back(el1); // earlier i have pushed cnt not el1 
+        }
+        if(cnt2 >= mini){
+            ls.push_back(el2); // here also put the condition 
+        }
         return ls;
     }
 };
