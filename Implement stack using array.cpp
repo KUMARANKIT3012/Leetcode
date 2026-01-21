a6f53b3aa117ae990227a663336ed4c7ec1698f0
Ankit Kumar <ankitkumar16729@gmail.com>
2026-01-21 20:55:07 +0530

    Update Implement stack using array.cpp

--- Implement stack using array.cpp
+++ Implement stack using array.cpp
@@ -1,11 +1,49 @@
-void MyStack ::push(int x) {
-    top = top + 1;
-    arr[top] = x;
-}
+class myStack {
+    private:
+    int *arr;
+    int top;
+    int size;
+    
+  public:
+    myStack(int n) {
+        // Define Data Structures
+        size = n;
+        arr = new int[n];
+        top = -1;
+    }
 
-int MyStack ::pop() {
-    if (top == -1) {
-        return -1;
-    } 
-    return arr[top--];
-}
+    bool isEmpty() {
+        // check if the stack is empty
+        return top == -1;
+    }
+
+    bool isFull() {
+        // check if the stack is full
+        return top == size-1;
+    }
+
+    void push(int x) {
+        // inserts x at the top of the stack
+        if(isFull()){
+            return;
+        }
+        top++;
+        arr[top] = x;
+    }
+
+    void pop() {
+        // removes an element from the top of the stack
+        if(isEmpty()){
+            return;
+        }
+        top--;
+    }
+
+    int peek() {
+        // Returns the top element of the stack
+        if(top == -1){
+            return -1;
+        }
+        return arr[top];
+    }
+};
