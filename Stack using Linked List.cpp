4425ca76a5741ec7097034b283ab6b071d65af42
Ankit Kumar <ankitkumar16729@gmail.com>
2026-01-24 19:21:37 +0530

    Refactor stack implementation with linked list
    Refactor stack implementation using a linked list. Updated class and method names, added size tracking, and implemented peek and isEmpty methods.

--- Stack using Linked List.cpp
+++ Stack using Linked List.cpp
@@ -1,27 +1,58 @@
-class MyStack {
-  private:
-    StackNode *top;
+/* class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+}; */
+
+class myStack {
+    private:
+    Node* topnode;
+    int cnt;
 
   public:
+    myStack() {
+        // Initialize your data members
+        topnode = nullptr;
+        cnt = 0;
+    }
+
+    bool isEmpty() {
+        // check if the stack is empty
+        return topnode == nullptr;
+    }
+
     void push(int x) {
-        // code here
-        StackNode* newnode = new StackNode(x);
-        newnode->next = top;
-        top = newnode;
+        // Adds an element x at the top of the stack
+        Node* newnode = new Node(x);
+        newnode->next = topnode;
+        topnode = newnode;
+        cnt++;
     }
 
-    int pop() {
-        // code here
-        if (top == NULL) {
-            return -1; 
-        }
-
-        int poppedValue = top->data;
-        StackNode* temp = top;
-        top = top->next; // Move top to the next node
+    void pop() {
+        // Removes the top element of the stack
+        if(isEmpty()) return;
+        
+        Node* temp = topnode;
+        topnode = topnode->next;
         delete temp;
-        return poppedValue;
+        cnt--;
     }
 
-    MyStack() { top = NULL; }
+    int peek() {
+        // Returns the top element of the stack
+        // If stack is empty, return -1
+        if(isEmpty()) return -1;
+        return topnode->data;
+    }
+
+    int size() {
+        // Returns the current size of the stack.
+        return cnt;
+    }
 };
