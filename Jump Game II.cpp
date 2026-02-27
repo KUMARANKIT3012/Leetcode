121db18413d5e13f56eff51a6bc82824587301e3
Ankit Kumar <ankitkumar16729@gmail.com>
2026-02-27 18:33:15 +0530

    Refactor jump function to use greedy approach

--- Jump Game II.cpp
+++ Jump Game II.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int solve(int ind, vector<int>& nums) {
-        int n = nums.size();
-        if (ind >= n - 1) return 0; // already at or beyond last index
-
-        int minJumps = INT_MAX;
-
-        for (int jump = 1; jump <= nums[ind]; jump++) {
-            int next = solve(ind + jump, nums);
-            if (next != INT_MAX)
-                minJumps = min(minJumps, 1 + next);
-        }
-
-        return minJumps;
-    }
-
     int jump(vector<int>& nums) {
-        return solve(0, nums);
+        int n = nums.size();
+        if(n <= 1) return 0;
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for(int i = 0; i<n-1; i++){
+            farthest = max(farthest, i + nums[i]);
+
+            if(i == currentEnd){
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+        return jumps;
     }
 };
