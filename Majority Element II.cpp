32a7734d643d1927f9dc1a7384901cf980bd6675
Ankit Kumar <ankitkumar16729@gmail.com>
2026-03-18 20:39:18 +0530

    Refactor majorityElement function for clarity

--- Majority Element II.cpp
+++ Majority Element II.cpp
@@ -2,46 +2,44 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int cnt1 = 0;
-        int cnt2 = 0;
+        int freq1 = 0;
+        int freq2 = 0;
         int el1 = INT_MIN;
         int el2 = INT_MIN;
-        for(int i = 0 ; i<n; i++){
-            if(cnt1 == 0 && nums[i] != el2){
-                cnt1 = 1;
+        for(int i = 0; i<n; i++){
+            if(freq1 == 0 && nums[i] != el2){
+                freq1 = 1;
                 el1 = nums[i];
             }
-            else if(cnt2 == 0 && nums[i] != el1){
-                cnt2 = 1;
+            else if(freq2 == 0 && nums[i] != el1){
+                freq2 = 1;
                 el2 = nums[i];
             }
-
-            else if(el1 == nums[i]) cnt1++;
-            else if(el2 == nums[i]) cnt2++;
+            else if(el1 == nums[i]) freq1++;
+            else if(el2 == nums[i]) freq2++;
             else{
-                cnt1--; // here i have done the mistake 
-                cnt2--; // here also 
+                freq1--;
+                freq2--;
             }
         }
 
+        vector<int> ans;
+        freq1 = 0;
+        freq2 = 0;
 
-        vector<int> ls;
-        cnt1 = 0;
-        cnt2 = 0;
-        for(int i =0 ; i<n ;i++){
-            if(nums[i] == el1) cnt1++; // here 
-            else if(nums[i] == el2){ // here also put the condition 
-                cnt2++;
-            }
+        for(int i = 0; i<n; i++){
+            if(el1 == nums[i]) freq1++;
+            else if(el2 == nums[i]) freq2++;
         }
 
         int mini = (int)(n/3)+1;
-        if(cnt1 >= mini){
-            ls.push_back(el1); // earlier i have pushed cnt not el1 
+
+        if(freq1 >= mini){
+            ans.push_back(el1);
         }
-        if(cnt2 >= mini){
-            ls.push_back(el2); // here also put the condition 
+        if(freq2 >= mini){
+            ans.push_back(el2);
         }
-        return ls;
+        return ans;
     }
 };
