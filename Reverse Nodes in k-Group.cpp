5fb4b8e9e0a2bc80329764ee2f874ca8ec1f9b18
Ankit Kumar <ankitkumar16729@gmail.com>
2026-04-01 21:15:51 +0530

    Refactor reverseKGroup and getKth methods

--- Reverse Nodes in k-Group.cpp
+++ Reverse Nodes in k-Group.cpp
@@ -1,55 +1,41 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* reverselinkedlist(ListNode* head) {
-        if(head == NULL || head->next == NULL) {
-            return head;
-        }
-        ListNode* newhead = reverselinkedlist(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return newhead;
-    }
-
-    ListNode* getkthnode(ListNode* temp, int k) {
-        k -= 1;
-        while(temp != NULL && k > 0) {
-            k--;
-            temp = temp->next;
-        }
-        return temp;
-    }
-
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* temp = head;
-        ListNode* prevlast = NULL;
-        
-        while(temp != NULL) {
-            ListNode* kthnode = getkthnode(temp, k);
-            if(kthnode == NULL) {
-                if(prevlast) prevlast->next = temp;
-                break;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* groupPrev = dummy;
+
+        while (true) {
+            ListNode* kth = getKth(groupPrev, k);
+            if (!kth) break;
+
+            ListNode* groupNext = kth->next;
+
+            // reverse group
+            ListNode* prev = kth->next;
+            ListNode* curr = groupPrev->next;
+
+            while (curr != groupNext) {
+                ListNode* tmp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = tmp;
             }
-            ListNode* nextnode = kthnode->next;
-            kthnode->next = NULL;
-            ListNode* newhead = reverselinkedlist(temp);
-            if(temp == head) {
-                head = newhead;
-            } else {
-                prevlast->next = newhead;
-            }
-            prevlast = temp;
-            temp = nextnode;
+
+            ListNode* tmp = groupPrev->next;
+            groupPrev->next = kth;
+            groupPrev = tmp;
         }
-        return head;
+
+        return dummy->next;
+    }
+
+private:
+    ListNode* getKth(ListNode* curr, int k) {
+        while (curr && k > 0) {
+            curr = curr->next;
+            k--;
+        }
+        return curr;
     }
 };
