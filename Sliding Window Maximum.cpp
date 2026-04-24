57f0ed828aa0b589bc470a1b5bd47001672a6e81
Ankit Kumar <ankitkumar16729@gmail.com>
2026-04-24 17:45:27 +0530

    Refactor maxSlidingWindow to use a different approach

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq; // Stores indices of elements
-        vector<int> result;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            // Remove elements out of the window from the front
-            if (!dq.empty() && dq.front() == i - k)
-                dq.pop_front();
-            
-            // Remove elements from back that are smaller than the current element
-            while (!dq.empty() && nums[dq.back()] <= nums[i])
-                dq.pop_back();
-            
-            // Insert current index
-            dq.push_back(i);
-            
-            // Store result once the first window is filled
-            if (i >= k - 1)
-                result.push_back(nums[dq.front()]);
+        vector<int> output;
+        deque<int> q;
+        int l = 0, r = 0;
+        while (r < nums.size()) {
+            while (!q.empty() && nums[q.back()] < nums[r]) {
+                q.pop_back();
+            }
+            q.push_back(r);
+            if (l > q.front()) {
+                q.pop_front();
+            }
+            if ((r + 1) >= k) {
+                output.push_back(nums[q.front()]);
+                l++;
+            }
+            r++;
         }
-        
-        return result;
+        return output;
     }
 };
