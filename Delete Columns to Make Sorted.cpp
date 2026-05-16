beaaa4736935c8bab03b5efcd88f9109311298f6
Ankit Kumar <ankitkumar16729@gmail.com>
2026-05-16 10:37:23 +0530

    Implement solution for deleting columns to sort strings

---
+++ Delete Columns to Make Sorted.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int rows = strs.size();
+        int cols = strs[0].size();
+
+        int count = 0;
+
+        for (int col = 0; col < cols; col++) {
+            for (int row = 1; row < rows; row++) {
+                if (strs[row][col] < strs[row - 1][col]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+};
