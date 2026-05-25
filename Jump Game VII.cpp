d215184c270dcc1cee6b13a183b2a2325d80e64b
Ankit Kumar <ankitkumar16729@gmail.com>
2026-05-25 12:10:09 +0530

    Implement Jump Game VII solution

---
+++ Jump Game VII.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        vector<bool> reachable(n, false);
+        reachable[0] = true;
+        int reachableCount = 0;
+        for (int i = 1; i < n; i++) {
+            if (i - minJump >= 0 && reachable[i - minJump]) {
+                reachableCount++;
+            }
+            if (i - maxJump - 1 >= 0 && reachable[i - maxJump - 1]) {
+                reachableCount--;
+            }
+            if (s[i] == '0' && reachableCount > 0) {
+                reachable[i] = true;
+            }
+        }
+        return reachable[n - 1];
+    }
+};
