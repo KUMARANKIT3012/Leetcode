1c897d8804d7dc6c94639ae42fae25d04fa4393c
Ankit Kumar <ankitkumar16729@gmail.com>
2026-06-08 14:46:53 +0530

    Update Binary Tree Maximum Path Sum.cpp

--- Binary Tree Maximum Path Sum.cpp
+++ Binary Tree Maximum Path Sum.cpp
@@ -14,3 +14,26 @@ public:
         return max(left, right) + node->val;
     }
 };
+
+
+
+
+// another soln : 
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int res = root->val;
+        function<int(TreeNode*)> dfs = [&](TreeNode* root) {
+            if(!root)
+                return 0;
+            int leftMax = dfs(root->left);
+            int rightMax = dfs(root->right);
+            leftMax = max(leftMax, 0);
+            rightMax = max(rightMax, 0);
+            res = max(res, root->val + leftMax + rightMax);
+            return root->val + max(leftMax, rightMax);
+        };
+        dfs(root);
+        return res;
+    }
+};
