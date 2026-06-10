907f7cb8e71a55be8fceff604ea457dace1b8968
Ankit Kumar <ankitkumar16729@gmail.com>
2026-06-10 13:18:37 +0530

    Implement function to connect next right pointers

---
+++ Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        Node* cur = root;
+        Node* nxt = root ? root->left : nullptr;
+        while (cur && nxt) {
+            cur->left->next = cur->right;
+            if (cur->next) {
+                cur->right->next = cur->next->left;
+            }
+            cur = cur->next;
+            if (!cur) {
+                cur = nxt;
+                nxt = cur->left;
+            }
+        }
+        return root;
+    }
+};
