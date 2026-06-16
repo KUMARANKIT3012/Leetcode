7c7b5d68400a04280edc6dc69429ea48f15fb23a
Ankit Kumar <ankitkumar16729@gmail.com>
2026-06-16 12:52:38 +0530

    Update Course Schedule.cpp

--- Course Schedule.cpp
+++ Course Schedule.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
+    unordered_set<int> visitSet;
+    bool dfs(int crs, vector<vector<int>>& preMap) {
+        if (visitSet.count(crs))
+            return false;
+        if (preMap[crs].empty())
+            return true;
+        visitSet.insert(crs);
+        for (int pre : preMap[crs]) {
+            if (!dfs(pre, preMap))
+                return false;
+        }
+        visitSet.erase(crs);
+        preMap[crs].clear();
+        return true;
+    }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // Step 1: Build adjacency list
-        vector<vector<int>> adj(numCourses);
-        for (auto &p : prerequisites) {
-            adj[p[1]].push_back(p[0]); // edge: p[1] -> p[0]
+        vector<vector<int>> preMap(numCourses);
+        for (auto& p : prerequisites) {
+            preMap[p[0]].push_back(p[1]);
         }
-
-        // Step 2: Calculate indegree for each node
-        vector<int> indegree(numCourses, 0);
-        for (int i = 0; i < numCourses; i++) {
-            for (auto it : adj[i]) {
-                indegree[it]++;
-            }
+        for (int crs = 0; crs < numCourses; crs++) {
+            if (!dfs(crs, preMap))
+                return false;
         }
-
-        // Step 3: Initialize queue with nodes having indegree 0
-        queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) q.push(i);
-        }
-
-        // Step 4: Topological Sort
-        vector<int> topo; // will store the topological order
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-
-            for (auto it : adj[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0) q.push(it);
-            }
-        }
-
-        // Step 5: If topo sort contains all courses → possible to finish
-        return topo.size() == numCourses;
+        return true;
     }
 };
