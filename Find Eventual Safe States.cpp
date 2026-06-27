504d047750ddca438e5a642210d0cdd7d296e03d
Ankit Kumar <ankitkumar16729@gmail.com>
2026-06-27 12:49:54 +0530

    Refactor eventualSafeNodes to use DFS approach

--- Find Eventual Safe States.cpp
+++ Find Eventual Safe States.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
+    unordered_map<int, bool> safe;
+    bool dfs(int node, vector<vector<int>>& graph) {
+        if (safe.count(node))
+            return safe[node];
+
+        safe[node] = false;
+        for (int nei : graph[node]) {
+            if (!dfs(nei, graph))
+                return safe[node];
+        }
+        safe[node] = true;
+        return true;
+    }
+
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        
-        // Step 1: Reverse the edges
-        vector<vector<int>> revGraph(n);
-        vector<int> indegree(n, 0);
-        
-        for (int u = 0; u < n; u++) {
-            for (int v : graph[u]) {
-                revGraph[v].push_back(u);
-                indegree[u]++; // indegree in reversed graph
-            }
+        vector<int> ans;
+        for (int i = 0; i < graph.size(); i++) {
+            if (dfs(i, graph))
+                ans.push_back(i);
         }
-        
-        // Step 2: Queue for topo sort
-        queue<int> q;
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) q.push(i);
-        }
-        
-        vector<int> safeNodes;
-        
-        // Step 3: Kahn's algorithm
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            safeNodes.push_back(node);
-            
-            for (int neigh : revGraph[node]) {
-                indegree[neigh]--;
-                if (indegree[neigh] == 0) {
-                    q.push(neigh);
-                }
-            }
-        }
-        
-        // Step 4: Sort result (because BFS may not give sorted order)
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+        return ans;
     }
 };
