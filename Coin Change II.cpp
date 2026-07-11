b0b7ad467229e0855d918c839f9509d9b19b53b1
Ankit Kumar <ankitkumar16729@gmail.com>
2026-07-11 14:22:12 +0530

    Update Coin Change II.cpp

--- Coin Change II.cpp
+++ Coin Change II.cpp
@@ -1,79 +1,20 @@
-// MEMOIZATION - 
-class Solution {
-public:
-    int f(int ind, int target, vector<int>& coins, vector<vector<int>>& dp) {
-        if (ind == 0) {
-            return (target % coins[0] == 0); // only one type of coin left
-        }
-        if (dp[ind][target] != -1) return dp[ind][target];
-
-        int notTake = f(ind - 1, target, coins, dp);
-        int take = 0;
-        if (coins[ind] <= target) 
-            take = f(ind, target - coins[ind], coins, dp);
-
-        return dp[ind][target] = take + notTake;
-    }
-
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        return f(n - 1, amount, coins, dp);
-    }
-};
-
-
-
-// TABULATION - 
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        int numCoins = coins.size();
+        int targetAmount = amount;
+        unsigned dp[numCoins + 1][targetAmount + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
 
-        // Base case: using only coins[0]
-        for (int t = 0; t <= amount; t++) {
-            if (t % coins[0] == 0) dp[0][t] = 1;
-        }
-
-        for (int ind = 1; ind < n; ind++) {
-            for (int target = 0; target <= amount; target++) {
-                int notTake = dp[ind-1][target];
-                int take = 0;
-                if (coins[ind] <= target) take = dp[ind][target - coins[ind]];
-                dp[ind][target] = take + notTake;
+        for (int i = 1; i <= numCoins; ++i) {
+            for (int j = 0; j <= targetAmount; ++j) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= coins[i - 1]) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                }
             }
         }
-        return dp[n-1][amount];
+        return dp[numCoins][targetAmount];
     }
 };
-
-
-
-
-
-// SPACE OPTIMIZED - 
-class Solution {
-public:
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<int> prev(amount + 1, 0), cur(amount + 1, 0);
-
-        // Base case
-        for (int t = 0; t <= amount; t++) {
-            if (t % coins[0] == 0) prev[t] = 1;
-        }
-
-        for (int ind = 1; ind < n; ind++) {
-            for (int target = 0; target <= amount; target++) {
-                int notTake = prev[target];
-                int take = 0;
-                if (coins[ind] <= target) take = cur[target - coins[ind]];
-                cur[target] = take + notTake;
-            }
-            prev = cur;
-        }
-        return prev[amount];
-    }
-};
-
