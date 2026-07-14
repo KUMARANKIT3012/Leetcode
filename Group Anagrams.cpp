9471c488370c091e81afc222a03939de37cca49f
Ankit Kumar <ankitkumar16729@gmail.com>
2026-07-14 21:17:54 +0530

    Refactor groupAnagrams to use sorting for keys

--- Group Anagrams.cpp
+++ Group Anagrams.cpp
@@ -2,27 +2,16 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> res;
-
-        for (auto &s : strs) {
-            vector<int> count(26, 0);
-
-            for (char c : s) {
-                count[c - 'a']++;
-            }
-
-            string key = "";
-            for (int i = 0; i < 26; i++) {
-                key += to_string(count[i]) + ",";
-            }
-
-            res[key].push_back(s);
+        for(const auto& s : strs){
+            string sortedS = s;
+            sort(sortedS.begin(), sortedS.end());
+            res[sortedS].push_back(s);
         }
 
-        vector<vector<string>> ans;
-        for (auto &it : res) {
-            ans.push_back(it.second);
+        vector<vector<string>>result;
+        for(auto&pair : res){
+            result.push_back(pair.second);
         }
-
-        return ans;
+        return result;
     }
 };
