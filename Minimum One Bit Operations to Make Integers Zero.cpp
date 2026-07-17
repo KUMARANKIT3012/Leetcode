ccf89bf239e00d8b0eaa4ece1cab2da65ae80c28
Ankit Kumar <ankitkumar16729@gmail.com>
2026-07-17 18:06:08 +0530

    Create Minimum One Bit Operations to Make Integers Zero.cpp

---
+++ Minimum One Bit Operations to Make Integers Zero.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if (n == 0) return 0;
+
+        int k = 31 - __builtin_clz(n);
+        int msb = 1 << k;
+
+        return ((1 << (k + 1)) - 1) - minimumOneBitOperations(n ^ msb);
+    }
+};
