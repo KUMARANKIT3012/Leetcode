3cd1ec431f32efa93ccd4c78ff6eeef9b14255c5
Ankit Kumar <ankitkumar16729@gmail.com>
2026-07-18 17:44:40 +0530

    Implement isThree function to check divisors

---
+++ Three Divisors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int cnt = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                cnt++;
+                if (i != n / i) cnt++;
+            }
+        }
+        return cnt == 3;
+    }
+};
