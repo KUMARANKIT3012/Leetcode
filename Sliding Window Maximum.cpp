f39ccafe0493d2984befc8ce289a1be9891f41aa
Ankit Kumar <ankitkumar16729@gmail.com>
2026-07-22 11:02:05 +0530

    Optimize maxSlidingWindow using priority queue
    Refactored maxSlidingWindow function to use a priority queue for efficiency.

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> heap;
         vector<int> output;
-        deque<int> q;
-        int l = 0, r = 0;
-        while (r < nums.size()) {
-            while (!q.empty() && nums[q.back()] < nums[r]) {
-                q.pop_back();
+        for (int i = 0; i < nums.size(); i++) {
+            heap.push({nums[i], i});
+            if (i >= k - 1) {
+                while (heap.top().second <= i - k) {
+                    heap.pop();
+                }
+                output.push_back(heap.top().first);
             }
-            q.push_back(r);
-            if (l > q.front()) {
-                q.pop_front();
-            }
-            if ((r + 1) >= k) {
-                output.push_back(nums[q.front()]);
-                l++;
-            }
-            r++;
         }
         return output;
     }
