70ec9f448296f8f1daade0a977337ca906c0d84d
Ankit Kumar <ankitkumar16729@gmail.com>
2026-07-30 20:51:30 +0530

    Refactor findMedianSortedArrays for merging and median
    Refactored findMedianSortedArrays to merge two sorted arrays and find the median without using binary search.

--- Median of Two Sorted Arrays.cpp
+++ Median of Two Sorted Arrays.cpp
@@ -1,74 +1,22 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-        int n1 = a.size();
-        int n2 = b.size();
-
-        if (n1 > n2) return findMedianSortedArrays(b, a); // bin search hamesha chote array pe hoga 
-
-        int low = 0, high = n1;
-        int left = (n1 + n2 + 1) / 2;
-
-        while (low <= high) {
-            int mid1 = (low + high) >> 1;
-            int mid2 = left - mid1;
-
-            int l1 = (mid1 == 0) ? INT_MIN : a[mid1 - 1];
-            int l2 = (mid2 == 0) ? INT_MIN : b[mid2 - 1];
-
-            int r1 = (mid1 == n1) ? INT_MAX : a[mid1];
-            int r2 = (mid2 == n2) ? INT_MAX : b[mid2];
-
-            if (l1 <= r2 && l2 <= r1) {
-                if ((n1 + n2) % 2 == 1)
-                    return max(l1, l2);
-                else
-                    return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
-            }
-            else if (l1 > r2) {
-                high = mid1 - 1;
-            }
-            else {
-                low = mid1 + 1;
-            }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>result;
+        for(int i = 0; i<nums1.size(); i++){
+            result.push_back(nums1[i]);
         }
 
-        return 0.0; 
-    }
-};
+        for(int i = 0; i<nums2.size(); i++){
+            result.push_back(nums2[i]);
+        }
 
-
-
-
-
-
-
-
-// OR
-
-
-
-
-
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-        vector<int> merged;
-
-        // Step 1: Add elements of both arrays
-        for (int x : a) merged.push_back(x);
-        for (int x : b) merged.push_back(x);
-
-        // Step 2: Sort the merged array
-        sort(merged.begin(), merged.end());
-
-        int n = merged.size();
-
-        // Step 3: Find median
-        if (n % 2 == 1) {
-            return merged[n / 2];
-        } else {
-            return (merged[n / 2] + merged[n / 2 - 1]) / 2.0;
+        sort(result.begin(), result.end());
+        int n = result.size();
+        if(result.size() % 2 == 1){
+            return result[n/2];
+        }
+        else{
+            return (result[n/2] + result[n/2-1]) / 2.0;
         }
     }
 };
