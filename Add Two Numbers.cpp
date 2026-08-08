f183de1f5f20605db2e23fc216398eeb208f710b
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-08 12:43:59 +0530

    Update Add Two Numbers.cpp

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -1,70 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyhead = new ListNode(-1);
-        ListNode* curr = dummyhead;
-        ListNode* temp1 = l1;
-        ListNode* temp2 = l2;
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+
         int carry = 0;
-        while(temp1 != NULL || temp2 != NULL){
-            int sum = carry;
-            if(temp1) sum+= temp1->val;
-            if(temp2) sum+= temp2->val;
-            ListNode* newnode = new ListNode(sum%10);
-            carry = sum/10;
-            curr->next = newnode;
-            curr = curr->next;
 
-            if(temp1) temp1 = temp1->next;
-            if(temp2) temp2 = temp2->next;
+        while(l1 || l2 || carry){
+            int v1 = l1 ? l1->val : 0;
+            int v2 = l2 ? l2->val : 0;
+
+            int val = v1 + v2 + carry;
+            carry = val / 10;
+            val = val % 10;
+
+            cur->next = new ListNode(val);
+
+            cur = cur->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
         }
-        if(carry){
-            ListNode* newnode = new ListNode(carry);
-            curr->next = newnode;
-        }
-        return dummyhead->next;
-    }
-};
 
-
-
-
-// Recursive Version : 
-class Solution {
-public:
-    ListNode* solve(ListNode* l1, ListNode* l2, int carry){
-        // Base case
-        if(l1 == NULL && l2 == NULL && carry == 0){
-            return NULL;
-        }
-        int sum = carry;
-
-        if(l1) sum += l1->val;
-        if(l2) sum += l2->val;
-
-        ListNode* node = new ListNode(sum % 10);
-        
-        // Recursive call
-        node->next = solve(
-            l1 ? l1->next : NULL,
-            l2 ? l2->next : NULL,
-            sum / 10
-        );
-
-        return node;
-    }
-
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return solve(l1, l2, 0);
+        return dummy->next;
     }
 };
