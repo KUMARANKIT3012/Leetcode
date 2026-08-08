4148e19308a93501a0cd68d33f4698f6eea16ea5
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-08 12:26:27 +0530

    Refactor copyRandomList for improved clarity and efficiency
    Refactored the copyRandomList method to use a hashmap for deep copying nodes and connecting random pointers.

--- Copy List with Random Pointer.cpp
+++ Copy List with Random Pointer.cpp
@@ -1,51 +1,23 @@
-
 class Solution {
 public:
-    void insertcopyinbetween(Node* head){
-        Node* temp = head;
-        while(temp!=NULL){
-            Node* nextelement = temp->next;
-            Node* copy = new Node(temp->val);
-            copy->next = nextelement;
-            temp->next = copy;
-            temp = nextelement;
-        }
-    }
-
-    void connectrandompointers(Node* head){
-        Node* temp = head;
-        while(temp != NULL){
-            Node* copynode = temp->next;
-            if(temp->random){
-                copynode->random = temp->random->next;
-            }
-            else{
-                copynode->random = nullptr;
-            }
-            temp = temp->next->next;
-        }
-    }
-
-    Node* getdeepcopylist(Node* head){
-        Node* temp = head;
-        Node* dummynode = new Node(-1);
-        Node* res = dummynode;
-
-        while(temp != NULL){
-            // creating a new list
-            res->next = temp->next;
-            res = res->next;
-
-            //disconnecting and going back to initial stage of ll
-            temp->next = temp->next->next;
-            temp = temp->next;
-        }
-        return dummynode->next;
-    }
     Node* copyRandomList(Node* head) {
-        if(!head) return nullptr;
-        insertcopyinbetween(head);
-        connectrandompointers(head);
-        return getdeepcopylist(head);
+        if (head == NULL) return NULL;
+        unordered_map<Node*, Node*> oldToCopy;
+
+        Node* cur = head;
+        while (cur) {
+            oldToCopy[cur] = new Node(cur->val);
+            cur = cur->next;
+        }
+
+        cur = head;
+        while (cur) {
+            Node* copy = oldToCopy[cur];
+            copy->next = oldToCopy[cur->next];
+            copy->random = oldToCopy[cur->random];
+            cur = cur->next;
+        }
+
+        return oldToCopy[head];
     }
 };
