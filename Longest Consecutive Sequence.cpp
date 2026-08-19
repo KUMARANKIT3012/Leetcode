3bc8d2d6cc13bd89aa6e86a1278c3800dddc962f
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-19 21:35:27 +0530

    Refactor longestConsecutive method implementation
    Refactor longestConsecutive function to improve clarity and efficiency.

--- Longest Consecutive Sequence.cpp
+++ Longest Consecutive Sequence.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0) return 0;
-        int longest =1;
-        unordered_set<int> st;
-        for(int i=0; i<n; i++){
-            st.insert(nums[i]);
-        }
-        for(auto it:st){
-            if(st.find(it - 1) == st.end()){
-                int count = 1;
-                int x =it;
-                while(st.find(x+1) != st.end()){
-                    x = x+1;
-                    count =count+1;
+        unordered_set<int> st(nums.begin(), nums.end());
+        int ans = 0;
+
+        for(int num : st){
+            if(st.find(num - 1) == st.end()){
+                int curr = num;
+                int len = 1;
+
+                while(st.find(curr + 1) != st.end()){
+                    curr++;
+                    len++;
                 }
-                longest = max(longest, count);
+
+                ans = max(ans, len);
             }
         }
 
-        return longest ;
-
+        return ans;
     }
 };
