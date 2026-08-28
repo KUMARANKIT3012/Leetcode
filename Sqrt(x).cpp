034ec467edfd174630b78bf56115e74a538be891
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-28 10:42:29 +0530

    Update Sqrt(x).cpp

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int mySqrt(int x) {
         long long i = 0;
-
         while(i * i <= x){
             i++;
         }
-        return i-1;
+        return i - 1;
     }
 };
