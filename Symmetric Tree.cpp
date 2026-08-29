016c01161e9fc2f684b3de5898f5988896b449ff
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-29 14:49:37 +0530

    Update Symmetric Tree.cpp

--- Symmetric Tree.cpp
+++ Symmetric Tree.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        return root == NULL || isSymmetricHelp(root->left, root->right);
+    bool check(TreeNode* left, TreeNode* right){
+        if(left == NULL && right == NULL) return true;
+        if(left == NULL || right == NULL) return false;
+        if(left->val != right->val) return false;
+        return check(left->left, right->right) && check(left->right, right->left);
     }
-
-    bool isSymmetricHelp(TreeNode* left, TreeNode* right) {
-        if (left == NULL || right == NULL)
-            return left == right;
-
-        if (left->val != right->val)
-            return false;
-
-        return isSymmetricHelp(left->left, right->right) &&
-               isSymmetricHelp(left->right, right->left);
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        return check(root->left, root->right);
     }
 };
