8674a380d92f55a087c26319dbbe990d76b43176
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-30 14:26:11 +0530

    Update Top K Frequent Elements.cpp

--- Top K Frequent Elements.cpp
+++ Top K Frequent Elements.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        // Step 1: Count frequency
-        unordered_map<int, int> freq;
-        for (int num : nums) {
-            freq[num]++;
+        unordered_map<int, int> mp;
+        for(int a : nums){
+            mp[a]++;
         }
 
-        // Step 2: Min heap (frequency, element)
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>
-        > pq;
-
-        // Step 3: Process elements
-        for (auto it : freq) {
-            if (pq.size() < k) {
-                pq.push({it.second, it.first});
-            } else {
-                pq.push({it.second, it.first});
-                pq.pop(); // removes smallest freq (like heappushpop)
-            }
+        vector<pair<int, int>>ans;
+        for(const auto &p : mp){
+            ans.push_back({p.second, p.first});
         }
 
-        // Step 4: Extract result
-        vector<int> result;
-        while (!pq.empty()) {
-            result.push_back(pq.top().second);
-            pq.pop();
+        sort(ans.rbegin(), ans.rend());
+        vector<int>res;
+        for(int i = 0; i<k; i++){
+            res.push_back(ans[i].second);
         }
 
-        return result;
+        return res;
     }
 };
