b80578144079b0185f55fa297b464daab0e4d7b8
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-31 12:46:48 +0530

    Update Can Place Flowers.cpp

--- Can Place Flowers.cpp
+++ Can Place Flowers.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0;
-        for (int i = 0; i < flowerbed.size(); i++) {
-            if (flowerbed[i] == 0) {
-                bool prev = true;
-                if (i > 0 && flowerbed[i - 1] == 1) {
-                    prev = false;
-                }
-                bool next = true;
-                if (i < flowerbed.size() - 1 && flowerbed[i + 1] == 1) {
-                    next = false;
-                }
-                if (prev && next) {
-                    flowerbed[i] = 1;
-                    count++;
-                }
+        int s = flowerbed.size();
+        int countz = 0;
+        for(int i = 0; i < s; i++){            
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == s-1 || flowerbed[i+1] == 0)){
+                countz++;
+                flowerbed[i] = 1;
             }
         }
-        return count >= n;
+        if(countz >= n) return true;
+        else return false;
     }
 };
