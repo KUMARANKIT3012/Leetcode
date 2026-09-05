0d5488f58e3239e448891dc6211d810672d4b09d
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-05 19:32:40 +0530

    Update Palindrome Partitioning.cpp

--- Palindrome Partitioning.cpp
+++ Palindrome Partitioning.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string> path;
-        func(0, s, path, res);
-        return res;
+    bool isPalindrome(string s, int start, int end) {
+        while (start <= end) {
+            if (s[start] != s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
-    void func(int index, string s, vector<string> &path,vector<vector<string>> &res){
-        if(index == s.size()){
-            res.push_back(path);
+
+    void func(string s, int idx, vector<string>& path, vector<vector<string>>& ans) {
+        if (idx == s.length()) {
+            ans.push_back(path);
             return;
         }
-        for(int i=index; i<s.size(); i++){
-            if(ispalindrome(s,index, i)){
-                path.push_back(s.substr(index, i-index+1));
-                func(i+1, s, path, res);
+
+        for (int i = idx; i < s.length(); i++) {
+            if (isPalindrome(s, idx, i)) {
+                path.push_back(s.substr(idx, i - idx + 1));
+                func(s, i + 1, path, ans);
                 path.pop_back();
             }
         }
     }
-    bool ispalindrome(string s, int start, int end){
-        while(start<=end){
-            if(s[start++] != s[end--]) return false;
-        }
-        return true;
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> path;
+        func(s, 0, path, res);
+        return res;
     }
 };
