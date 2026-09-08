757542343e67894130a8581d9df7556d09f30111
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-08 20:41:17 +0530

    Update Set Matrix Zeroes.cpp

--- Set Matrix Zeroes.cpp
+++ Set Matrix Zeroes.cpp
@@ -3,42 +3,24 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        int col0 = 1;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+
+        vector<int>rows(m, 0);
+        vector<int>cols(n, 0);
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    // mark the i-th row:
-                    matrix[i][0] = 0;
-                    // mark the j-th col:
-                    if(j!=0){
-                        matrix[0][j] = 0;
-                    }
-                    else{
-                        col0 = 0;
-                    }
-
+                    rows[i] = 1;
+                    cols[j] = 1;
                 }
             }
         }
-        for(int i = 1; i<m; i++){ // solving 1st part
-            for(int j=1; j<n; j++){
-                if(matrix[i][j] != 0){
-                    // check for col and row:
-                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
-                        matrix[i][j] = 0;
-                    }
 
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(rows[i] || cols[j]){
+                    matrix[i][j] = 0;
                 }
-            }
-        }
-        if(matrix[0][0] == 0){ // solving 2nd part
-            for(int j=0; j<n; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        if(col0 == 0){ // solving 3rd part
-            for(int i=0; i<m; i++){
-                matrix[i][0] = 0;
             }
         }
     }
