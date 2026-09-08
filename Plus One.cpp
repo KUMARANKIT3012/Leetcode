3ef4ff72d0881bcb60dda72f12417d4e433f305b
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-08 20:48:51 +0530

    Refactor plusOne function to handle carry over

--- Plus One.cpp
+++ Plus One.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
@@ -10,8 +9,6 @@ public:
             }
             digits[i] = 0;
         }
-
-        // if all were 9
         digits.insert(digits.begin(), 1);
         return digits;
     }
