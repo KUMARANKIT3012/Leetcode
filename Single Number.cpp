fbb10d31320d52db72fdea6d5c3bd460265f1eb7
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-11 17:44:39 +0530

    Update Single Number.cpp

--- Single Number.cpp
+++ Single Number.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int val:nums){
-            ans=ans^val;
+        int ans = 0;
+        for(int a : nums){
+            ans ^= a;
         }
+
         return ans;
     }
 };
