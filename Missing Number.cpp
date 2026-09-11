e350832881070bd3a98964bed2969d07bc6cacab
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-11 17:56:19 +0530

    Update Missing Number.cpp

--- Missing Number.cpp
+++ Missing Number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n =nums.size();
-        int sum = n*(n+1)/2;
-        int s2 = 0;
-        for(int i=0; i<n; i++){
-            s2+=nums[i];
+        int n = nums.size();
+        int total = n * (n + 1)/2;
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            ans += nums[i];
         }
-        return (sum - s2);
+
+        return total - ans;
     }
 };
