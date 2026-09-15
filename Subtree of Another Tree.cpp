10e0fe7e4f7f3691809eb3315a1802b8e1ad185d
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-15 18:39:27 +0530

    Update Subtree of Another Tree.cpp

--- Subtree of Another Tree.cpp
+++ Subtree of Another Tree.cpp
@@ -25,3 +25,39 @@ public:
         isSubtree(root->right, subRoot);
     }
 };
+
+
+
+// optimized way of solving this : 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool sametree(TreeNode* s, TreeNode* t){
+        if(s == NULL || t == NULL){
+            return s == t;
+        }
+
+        return (s->val == t->val) && sametree(s->left, t->left) && sametree(s->right, t->right);
+    }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root) return false;
+
+        if(!subRoot) return true;
+
+        if(sametree(root, subRoot)){
+            return true;
+        }
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+};
