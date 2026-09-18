dd26757b3491a113dfbb9a447a6718a4bf1fe340
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-18 21:26:14 +0530

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,21 +1,21 @@
+// changes made 
 class Solution {
 public:
     vector<vector<int>> res;
-    vector<int> subset;
+    vector<int>subset;
 
-    void dfs(vector<int>& nums, int i) {
-        if (i >= nums.size()) {
+    void dfs(vector<int>& nums, int i){
+        if(i >= nums.size()){
             res.push_back(subset);
             return;
         }
 
         subset.push_back(nums[i]);
-        dfs(nums, i + 1);
+        dfs(nums, i+1);
 
         subset.pop_back();
-        dfs(nums, i + 1);
+        dfs(nums, i+1);
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
         dfs(nums, 0);
         return res;
