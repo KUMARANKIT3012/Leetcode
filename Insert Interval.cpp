3c0ebd4aba346136e9392cf09b6c8e95a8386f3c
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-21 20:29:16 +0530

    Refactor insert function for interval merging

--- Insert Interval.cpp
+++ Insert Interval.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> result;
-        int i = 0;
-        int n = intervals.size();
-
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            result.push_back(intervals[i]);
-            i++;
+        vector<vector<int>> res;
+        for (int i = 0; i < intervals.size(); i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                res.push_back(newInterval);
+                for (int j = i; j < intervals.size(); j++) {
+                    res.push_back(intervals[j]);
+                }
+                return res;
+            }
+            else if (newInterval[0] > intervals[i][1]) {
+                res.push_back(intervals[i]);
+            }
+            else {
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
         }
-
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        result.push_back(newInterval);
-
-        while (i < n) {
-            result.push_back(intervals[i]);
-            i++;
-        }
-
-        return result;
+        res.push_back(newInterval);
+        return res;
     }
 };
